95be5799eb66301c4a8c796968ed29d65ce17e89
kim <tkdguq9369@naver.com>
2022-07-31 22:44:12 +0900

    2022/07/31 2강 시큐리티 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.cos.security1.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -11,5 +12,35 @@ public class IndexController {
     @GetMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료";
     }
 }
